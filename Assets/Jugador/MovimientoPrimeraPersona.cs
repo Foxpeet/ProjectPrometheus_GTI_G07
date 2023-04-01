28a08ec28e0e733eb9e13402a6f11855245727c6
Laura Gavilán Fernández <lauragavilan3@gmail.com>
2023-04-01 20:35:00 +0200

    El jugador puede Esprintar
    El jugador puede correr en función de la stamina.

--- Assets/Jugador/MovimientoPrimeraPersona.cs
+++ Assets/Jugador/MovimientoPrimeraPersona.cs
@@ -2,21 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class MovimientoPrimeraPersona : MonoBehaviour {
 
-public class MovimientoPrimeraPersona : MonoBehaviour
-{
-
+    // Objeto CharacterController que controla el movimiento del jugador
     public CharacterController controller;
+
+    // Velocidad, por defecto, de movimiento normal
     public float speed = 12f;
 
-    // Update is called once per frame
-    void Update()
-    {
+    // Velocidad, por defecto, de movimiento al correr
+    public float runSpeed = 24f;
+
+    // Cantidad máxima de stamina que el jugador puede tener
+    public float maxStamina = 10f;
+
+    // Tasa, por defecto, de regeneración de la stamina
+    public float staminaRegenRate = 1f;
+
+    // Tecla para correr -> se puede modificar más adelante en la interfaz también
+    public KeyCode runKey = KeyCode.LeftShift;
+
+    // Booleano que indica si el jugador está corriendo
+    private bool isRunning = false;
+
+    // Cantidad actual de stamina del jugador
+    private float currentStamina;
+
+    void Start() {
+        // Establecer la cantidad inicial de stamina a la cantidad máxima
+        currentStamina = maxStamina;
+    }
+
+    void Update() {
+        // Obtener la entrada de movimiento horizontal y vertical
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        // Calcular la dirección del movimiento basada en la entrada
         Vector3 move = transform.right * x + transform.forward * z;
 
+        // Si el jugador está corriendo
+        if (isRunning) {
+            // Reducir la stamina mientras se corre
+            currentStamina -= Time.deltaTime;
+            if (currentStamina <= 0) {
+                // Si la stamina llega a cero, dejar de correr y volver a la velocidad normal
+                currentStamina = 0;
+                isRunning = false;
+                speed = 12f;
+            }
+        } else {
+            // Regenerar la stamina mientras no se corre
+            if (currentStamina < maxStamina) {
+                currentStamina += Time.deltaTime * staminaRegenRate;
+            }
+            // Si se presiona la tecla de correr y hay suficiente stamina
+            // La stamina mínima para empezar a correr es 0.1f unidades de tiempo
+            if (Input.GetKeyDown(runKey) && currentStamina >= 0.1f) {
+                // Empezar a correr y aumentar la velocidad
+                isRunning = true;
+                speed = runSpeed;
+            }
+        }
+
+        // Mover al jugador basado en la dirección y velocidad de movimiento
         controller.Move(move * speed * Time.deltaTime);
     }
 }
--- Assets/Jugador/RatonVista.cs
+++ Assets/Jugador/RatonVista.cs
@@ -5,26 +5,26 @@ using UnityEngine;
 public class RatonVista : MonoBehaviour
 {
 
-    public float mouseSensitivity = 100f;
-    public Transform playerBody;
-    public float xRotation = 0f;
+    public float mouseSensitivity = 100f; // La sensibilidad que va a tener el ratón por defecto
+    public Transform playerBody; // El cuerpo del jugador
+    public float xRotation = 0f; // La rotación en el eje X
 
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked; // Bloquea el cursor en el centro de la pantalla
     }
 
     // Update is called once per frame
     void Update()
     {
-     float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-     float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+     float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime; // El movimiento del ratón en el eje X
+     float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime; // El movimiento del ratón en el eje Y
 
-     xRotation -= mouseY;
-     xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+     xRotation -= mouseY; // La rotación en el eje X se actualiza con el movimiento del ratón en el eje Y
+     xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Se limita la rotación en el eje X a un máximo de 90 grados hacia arriba y hacia abajo
 
-     transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-     playerBody.Rotate(Vector3.up * mouseX);   
+     transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); // Se actualiza la rotación del objeto que tiene este script (la cámara) en función de la rotación en el eje X
+     playerBody.Rotate(Vector3.up * mouseX); // Se rota el cuerpo del jugador en función del movimiento del ratón en el eje X
     }
 }
