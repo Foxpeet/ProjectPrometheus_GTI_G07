49bc5bde4d6cb590c36172954b1528db09b2aee8
jorge morcillo aviles <jjorgemaviles@gmail.com>
2023-04-28 15:21:06 +0200

    Menú principal

---
+++ Assets/Menus/Principal/scripts/AnimacionMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimacionMenu : MonoBehaviour
+{
+    float mousePosX;
+
+    float mousePosY;
+
+    
+    void Start()
+    {
+
+    }
+
+
+
+
+    void Update()
+    {
+
+        mousePosX = Input.mousePosition.x;
+        mousePosY = Input.mousePosition.y;
+
+        this.GetComponent<RectTransform>().position = new Vector2(
+            (mousePosX / Screen.width) * 20 + (Screen.width / 2),
+            (mousePosY / Screen.height) * 20 + (Screen.height / 2));
+    }
+}
